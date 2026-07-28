e371c7e7b145852d9dbd0da8b9f0b7faa7efbcff
BERNARDEAU <camil86100@gmail.com>
2026-07-28 13:04:18 +0200

    Initial commit - partie 3 phpUnit

---
+++ tests/init.php
@@ -0,0 +1,11 @@
+<?php
+// ce fichier est chargé par php unit avant chaque test
+// IL charge toutes les classes necessaires pour les tests unitaires
+
+require_once __DIR__ . '/../classes/config.php';
+require_once __DIR__ . '/../classes/DAO.class.php';
+require_once __DIR__ . '/../classes/Article.class.php';
+require_once __DIR__ . '/../classes/Empruntable.class.php';
+require_once __DIR__ . '/../classes/Livre.class.php';
+require_once __DIR__ . '/../classes/Dvd.class.php';
+
